f313011ac20ba8325a4b6d71a92436247bb57a79
Erick Guan <fantasticfears@gmail.com>
2015-06-14 07:50:22 +0800

    fix sass pipeline

--- client/src/config.js
+++ client/src/config.js
@@ -9,7 +9,7 @@ System.config({
 
 System.config({
   "map": {
-    "bootstrap": "github:twbs/bootstrap-sass@3.3.4",
+    "bootstrap-sass": "github:twbs/bootstrap-sass@3.3.4",
     "jquery": "github:components/jquery@2.1.4",
     "traceur": "github:jmcriffey/bower-traceur@0.0.88",
     "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88"
